eac141bf1e5d7e105343ebbc97e198b546c63df4
sergeyzoloto <sergiozoloto@gmail.com>
2023-03-11 13:29:23 +0100

    initial clean up and drafts

---
+++ src/components/TransactionList/TransactionList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Transaction } from './Transaction';
+
+export const TransactionList = () => {
+  return (
+    <>
+      <div>TransactionList</div>
+      <Transaction />
+      <Transaction />
+      <Transaction />
+    </>
+  );
+};
