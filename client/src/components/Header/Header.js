8ee0237d2b6932c4f1e6a31891e371936c678a4b
Sergey Zolotko <sergiozoloto@gmail.com>
2023-04-17 18:18:34 +0200

    useLogout hook

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useLogout } from '../../hooks/useLogout';
 
 export const Header = () => {
-  return <h1>Finance Tracker</h1>;
+  const { logout } = useLogout();
+  const handleClick = () => {
+    logout();
+  };
+
+  return (
+    <header>
+      <Link to="/">
+        <h1>Finance Tracker</h1>
+      </Link>
+      <nav>
+        <div>
+          <button onClick={handleClick}>Log out</button>
+        </div>
+        <div>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/login">Login</Link>
+        </div>
+      </nav>
+    </header>
+  );
 };
