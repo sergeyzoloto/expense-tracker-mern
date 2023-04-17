1b9cbd2407ff8e0cee5380b0f47c7ebc3dc9f11c
Sergey Zolotko <sergiozoloto@gmail.com>
2023-04-17 14:01:41 +0200

    email and password validation

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import validator from 'validator';
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -14,6 +16,17 @@ const UserSchema = new mongoose.Schema({
 
 // static signup static method
 UserSchema.statics.signup = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw new Error('Email and password are required');
+  }
+  if (!validator.isEmail(email)) {
+    throw new Error('Invalid email');
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw new Error('Password is not strong enough');
+  }
+
   const exists = await this.findOne({ email });
 
   if (exists) {
@@ -24,6 +37,7 @@ UserSchema.statics.signup = async function (email, password) {
   const hash = await bcrypt.hash(password, salt);
 
   const user = await this.create({ email, password: hash });
+  return user;
 };
 
 export default mongoose.model('User', UserSchema);
