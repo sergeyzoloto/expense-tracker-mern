3220c331a740adef927e6fc1f818ce0d8933ffc9
Sergey Zolotko <sergiozoloto@gmail.com>
2023-04-17 21:12:34 +0200

    protecting react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
 import './App.css';
 import { Header } from './components/Header/Header';
-import { GlobalProvider } from './context/GlobalState';
-import { AuthContextProvider } from './context/AuthContext';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home.js';
 import Login from './pages/Login.js';
 import Signup from './pages/Signup.js';
+import { useAuthContext } from './hooks/useAuthContext';
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
-    <AuthContextProvider>
-      <GlobalProvider>
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </BrowserRouter>
-      </GlobalProvider>
-    </AuthContextProvider>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!user ? <Signup /> : <Navigate to="/" />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
