a86df15b8efbc5f07802ea24668075ebb64ceeba
Sergey Zolotko <sergiozoloto@gmail.com>
2023-04-17 18:38:45 +0200

    setting the initial auth status

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useLogout } from '../../hooks/useLogout';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 export const Header = () => {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
+
   const handleClick = () => {
     logout();
   };
@@ -14,13 +17,18 @@ export const Header = () => {
         <h1>Finance Tracker</h1>
       </Link>
       <nav>
-        <div>
-          <button onClick={handleClick}>Log out</button>
-        </div>
-        <div>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/login">Login</Link>
-        </div>
+        {user && (
+          <div>
+            <span>{user.email}</span>
+            <button onClick={handleClick}>Log out</button>
+          </div>
+        )}
+        {!user && (
+          <div>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/login">Login</Link>
+          </div>
+        )}
       </nav>
     </header>
   );
--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer, useEffect } from 'react';
 
 export const AuthContext = createContext();
 
@@ -21,6 +21,15 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
   });
+
+  // check if the user is logged in
+  // if so, update the auth context
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      dispatch({ type: 'LOGIN', payload: user });
+    }
+  }, []);
 
   console.log('AuthContextProvider state', state);
 
