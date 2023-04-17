b4a08bfe37d84ac152e5cc5ff39e442626443235
Sergey Zolotko <sergiozoloto@gmail.com>
2023-04-17 17:12:30 +0200

    login and signup forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
 import { Header } from './components/Header/Header';
-import { Balance } from './components/Balance/Balance';
-import { IncomeExpenses } from './components/IncomeExpenses./IncomeExpenses';
-import { TransactionList } from './components/TransactionList/TransactionList';
-import { AddNewTransaction } from './components/AddNewTransaction/AddNewTransaction';
 import { GlobalProvider } from './context/GlobalState';
 import { AuthContextProvider } from './context/AuthContext';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.js';
+import Login from './pages/Login.js';
+import Signup from './pages/Signup.js';
 
 function App() {
   return (
     <AuthContextProvider>
       <GlobalProvider>
-        <Header />
-        <div className="container">
-          <Balance />
-          <IncomeExpenses />
-          <TransactionList />
-          <AddNewTransaction />
-        </div>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
       </GlobalProvider>
     </AuthContextProvider>
   );
